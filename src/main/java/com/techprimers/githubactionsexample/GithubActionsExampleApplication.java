cc67d9b55fbf0ef0dc08afc84d3d19ad9ca778b2
agaananda <saidanandaa@gmail.com>
2022-06-29 12:58:50 +0700

    contoh

--- src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
+++ src/main/java/com/techprimers/githubactionsexample/GithubActionsExampleApplication.java
@@ -14,7 +14,6 @@ public class GithubActionsExampleApplication {
 		SpringApplication.run(GithubActionsExampleApplication.class, args);
 		SpringApplication.run(GithubActionsExampleApplication.class, args);
 		SpringApplication.run(GithubActionsExampleApplication.class, args);
-		SpringApplication.run(GithubActionsExampleApplication.class, args);
 	}
 	}
 
